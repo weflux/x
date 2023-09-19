754d23f0d62a60c582567495485aa81dc1a53b72
liqiulin <liqiulin@outlook.com>
2023-09-19 20:37:25 +0800

    update: structs

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
---
+++ structs/convert.go
@@ -0,0 +1,22 @@
+package structs
+
+import "github.com/mitchellh/mapstructure"
+
+func MapTo(input interface{}, output interface{}, tagName ...string) error {
+	tag := "redis"
+	if len(tagName) > 1 {
+		tag = tagName[0]
+	}
+	config := &mapstructure.DecoderConfig{
+		WeaklyTypedInput: true,
+		Result:           output,
+		TagName:          tag,
+	}
+
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+
+	return decoder.Decode(input)
+}
